941eb377b942354a672a926b61676515dedc2892
khemagarwal <34685198+khemagarwal@users.noreply.github.com>
2020-06-06 11:35:52 +0530

    Breaking the Record

---
+++ Practice/Algorithms/Implementation/Breakingtherecord.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+        int highscore=scores[0];
+        int lowscore=scores[0];
+        int lowcount=0;
+        int highcount=0;
+
+
+        for(int i=1;i<scores.length;i++)
+        {
+            if(scores[i] > highscore)
+            {
+                highcount++;
+                highscore = scores[i];
+            }
+            else if(scores[i] < lowscore)
+            {
+                lowcount++;
+                lowscore = scores[i];
+            }
+
+        }
+         return new int[] {highcount,lowcount};
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] scores = new int[n];
+
+        String[] scoresItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresItems[i]);
+            scores[i] = scoresItem;
+        }
+
+        int[] result = breakingRecords(scores);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
